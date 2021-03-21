39c944b3ccf988bf34a6f976e6ce54d474190eda
Mandy <mandykparson@gmail.com>
2021-03-21 12:01:31 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = "Mandy Parson is the champion"
+document.body.appendChild(newHeader)
